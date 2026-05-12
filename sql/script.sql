ff952b3b9fca8856baa4e6efc1278354dcd9f0c9
kurachi <matheuskurachi6@gmail.com>
2026-05-12 08:02:04 -0300

    Add SQL script to create database and tables for hotel management system

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,36 @@
-// script //
+CREATE DATABASE hotelaria;
+
+USE hotelaria;
+
+CREATE TABLE hospedes (
+
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NULL,
+    telefone VARCHAR(100) NULL,
+    cpf VARCHAR(14) UNIQUE
+
+);
+
+CREATE TABLE quartos (
+
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    numero VARCHAR(10) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    valor_diaria DECIMAL(10,2) NOT NULL,
+    status VARCHAR(20) NOT NULL
+
+);
+
+CREATE TABLE reservas (
+	
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    hospede_id INT,
+    quarto_id INT,
+    data_entrada DATE NOT NULL,
+    data_saida DATE NOT NULL,
+    
+    FOREIGN KEY (hospede_id) REFERENCES hospedes(id),
+    FOREIGN KEY (quarto_id) REFERENCES quartos(id)
+    
+);
